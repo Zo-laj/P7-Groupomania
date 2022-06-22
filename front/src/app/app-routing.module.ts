f640fcfafd17555d08d3847f876f2cb1d8a52ea2
zo-laj <zoe.lajoie2@gmail.com>
2022-06-22 10:47:34 +0200

    création des fichiers des composants, des services et création path dans app router module

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { PostFormComponent } from './post-form/post-form.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { SinglePostComponent } from './single-post/single-post.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'auth/signup', component: SignupComponent},
+  { path: 'auth/login', component: LoginComponent},
+  { path: 'posts', component: PostListComponent},
+  { path: 'post/:id', component: SinglePostComponent},
+  { path: 'new-post', component: PostFormComponent},
+  { path: 'modify-post/:id', component: PostFormComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,16 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { SinglePostComponent } from './single-post/single-post.component';
+import { PostFormComponent } from './post-form/post-form.component';
+import { HeaderComponent } from './header/header.component';
+import { AuthGardService } from './services/auth-gard.service';
+import { AuthService } from './services/auth.service';
+import { PostsService } from './services/posts.service';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    PostListComponent,
+    SinglePostComponent,
+    PostFormComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    AuthService,
+    PostsService,
+    AuthGardService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
